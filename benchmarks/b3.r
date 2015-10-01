7b2cd2fdc2aa8d214829ee5e084b37040664dce8
Diego Giovane Pasqualin <dpasqualin@c3sl.ufpr.br>
2015-10-01 16:24:55 -0300

    Modify b3.r
    Signed-off-by: Diego Giovane Pasqualin <dpasqualin@c3sl.ufpr.br>

--- benchmarks/b3.r
+++ benchmarks/b3.r
@@ -4,8 +4,11 @@
 require(sfreemap.tests)
 require(sfreemapc)
 
-q <- seq(2,20,2)
+q <- seq(2,10,2)
 n_testes <- 5
 
 output <- paste('sfreemap', 'omp', 'single-tree', 'txt', sep='.')
 res<-sfreemap.test.perf(1, 128, q, prog='sfreemapc', n_tests=5, omp=TRUE, file=output)
+
+output <- paste('sfreemap', 'serial', 'single-tree', 'txt', sep='.')
+res<-sfreemap.test.perf(1, 128, q, prog='sfreemapc', n_tests=5, file=output)
