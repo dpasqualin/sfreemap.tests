c85aeddc37bb7ccf5562f0d0dea05dadd5dc9d1f
Diego Giovane Pasqualin <dpasqualin@gmail.com>
2015-11-08 22:14:17 -0200

    rename and improve b1.r

---
+++ benchmarks/test_increasing_parameters.r
@@ -0,0 +1,41 @@
+#!/usr/bin/env Rscript
+
+require(sfreemap.tests)
+require(sfreemap)
+require(sfreemapc)
+
+# constants
+progs = c('sfreemap', 'sfreemapc', 'simmap')
+n_tests = 7
+
+run <- function(trees, taxa, q, criteria) {
+    for (p in progs) {
+        output <- paste(p, criteria, 'txt', sep='.')
+        sfreemap.test.perf(trees
+                            , taxa
+                            , q
+                            , prog=p
+                            , n_tests=n_tests
+                            , file=output)
+    }
+}
+
+#
+# increasing states
+#
+q <- seq(2,20,2)
+run(1, 128, q, 'states')
+
+#
+# increasing trees
+#
+trees <- c(1,seq(2,128,2))
+run(trees, 128, 4, 'trees') # serial
+
+#
+# increasing taxa
+#
+taxa <- seq(32,1024,32)
+run(1, taxa, 4, 'taxa')
+
+print("DONE")
