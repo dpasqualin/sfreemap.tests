09c060d66d06dffb525c3d3e9aa238c887a5f4a8
Diego Giovane Pasqualin <dpasqualin@gmail.com>
2015-11-28 18:21:32 -0200

    Add file with code used to generate figures for my article

---
+++ R/for_my_article.R
@@ -0,0 +1,42 @@
+just_do_it <- function() {
+
+    # generate grouwth rate chart
+    x <- list(
+        files=c('sfreemapc.taxa.txt', 'sfreemapc.states.txt',
+                'sfreemapc.trees.txt', 'simmap.states.txt'),
+        types=c('taxa', 'state', 'tree', 'state'),
+        legend=c('Sfreemap-Taxa', 'Sfreemap-Estados',
+                 'Sfreemap-Árvores', 'Simmap-Estados')
+    )
+
+    # limit=10 because we only have 10 values for states
+    plot_growth_rate(x, 10, output='/tmp/growth.png')
+
+    # ----------------------------------------------------------
+    x <- list(
+        files=c('sfreemapc.trees.txt', 'sfreemap.trees.txt'),
+        types=c('tree', 'tree'),
+        legend=c('SFREEMAP-C', 'SFREEMAP-R')
+    )
+
+    plot_comparison(x, "Número de árvores", output='/tmp/trees_serial.png')
+
+    # ----------------------------------------------------------
+    x <- list(
+        files=c('sfreemapc.taxa.txt', 'sfreemap.taxa.txt'),
+        types=c('taxa', 'taxa'),
+        legend=c('SFREEMAP-C', 'SFREEMAP-R')
+    )
+
+    plot_comparison(x, "Número de taxa", output='/tmp/taxa_serial.png')
+
+    # ----------------------------------------------------------
+    x <- list(
+        files=c('sfreemapc.states.txt', 'sfreemap.states.txt'),
+        types=c('state', 'state'),
+        legend=c('SFREEMAP-C', 'SFREEMAP-R')
+    )
+
+    plot_comparison(x, "Número de estados do caráter", output='/tmp/states_serial.png')
+
+}
