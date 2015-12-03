c74a90cf708946de004b8c5408aaa394ec04e568
Diego Giovane Pasqualin <dpasqualin@c3sl.ufpr.br>
2015-12-03 09:45:45 -0200

    Update experiments data
    Signed-off-by: Diego Giovane Pasqualin <dpasqualin@c3sl.ufpr.br>

--- benchmarks/test_increasing_parameters.r
+++ benchmarks/test_increasing_parameters.r
@@ -5,12 +5,15 @@ require(sfreemap)
 require(sfreemapc)
 
 # constants
-progs = c('sfreemap', 'sfreemapc', 'simmap')
-n_tests = 7
+n_tests <- 5
+nsim <- c(1,10,100)
 
-run <- function(trees, taxa, q, criteria, nsim=c(1)) {
+run <- function(trees, taxa, q, criteria, nsim=c(1)
+                     , parallel = TRUE
+                     , progs = c('sfreemap', 'sfreemapc', 'simmap')) {
     for (p in progs) {
-        parallel <- ifelse(p == 'simmap', FALSE, TRUE)
+        parallel <- ifelse(p == 'simmap', FALSE, parallel)
+        nsim <- ifelse(p == 'simmap', nsim, c(1))
         output <- paste(p, criteria, 'txt', sep='.')
         sfreemap.test.perf(trees
                             , taxa
@@ -27,18 +30,18 @@ run <- function(trees, taxa, q, criteria, nsim=c(1)) {
 # increasing states
 #
 q <- seq(2,20,2)
-run(1, 128, q, 'states')
+run(1, 128, q, 'states', parallel=FALSE, nsim=nsim)
 
 #
 # increasing trees
 #
 trees <- c(1,seq(2,128,2))
-run(trees, 128, 4, 'trees') # serial
+run(trees, 128, 4, 'trees', nsim=nsim)
 
 #
 # increasing taxa
 #
 taxa <- seq(32,1024,32)
-run(1, taxa, 4, 'taxa')
+run(1, taxa, 4, 'taxa', nsim=nsim)
 
 print("DONE")
